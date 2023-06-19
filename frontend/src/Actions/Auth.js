d5c52df4ca02d7d7bea6098738e14ff7f2ed1c5d
amir_tardasti <amir.tardasti7@gmail.com>
2023-06-19 20:28:39 +0430

    work on front end about auth and user bar

--- frontend/src/Actions/Auth.js
+++ frontend/src/Actions/Auth.js
@@ -1,7 +1,8 @@
 import fetch from 'isomorphic-fetch';
 import { API } from '../config';
 import _fetch from 'isomorphic-fetch';
-
+import cookie from 'js-cookie';
+import process from 'process'
 
 export const register = (user) => {
 
@@ -33,4 +34,76 @@ export const login = (user) => {
         return response.json()
 
     }).catch(error => console.log(error))
+}
+
+export const signOut = (next) => {
+
+    removeCookie('token');
+    removeLocalStorage('user');
+    next();
+
+    return _fetch(`${API}/signout`, {
+        method: 'GET'
+    }).then(response => {
+        console.log('sign out successfuly');
+    }).catch(error => {
+        console.log(error);
+    })
+
+}
+
+export const setCookie = (key, value) => {
+
+    if (process.browser) {
+        cookie.set(key, value, {
+            expires: 1
+        })
+    }
+}
+
+export const getCookie = (key) => {
+
+    if (process.browser) {
+        return cookie.get(key);
+    }
+}
+
+export const removeCookie = (key) => {
+
+    if (process.browser) {
+        cookie.remove(key, {
+            expires: 1
+        })
+    }
+}
+
+export const setLocalStorage = (key, value) => {
+    if (process.browser) {
+        localStorage.setItem(key, JSON.stringify(value))
+    }
+}
+
+export const removeLocalStorage = (key) => {
+    if (process.browser) {
+        localStorage.removeItem(key)
+    }
+}
+
+export const authenticate = (data, next) => {
+    setCookie('token', data.token)
+    setLocalStorage('user', data.user)
+    next();
+}
+
+export const isAuth = () => {
+    if (process.browser) {
+        const cookieChecked = getCookie('token')
+        if (cookieChecked) {
+            if (localStorage.getItem('user')) {
+                return JSON.parse(localStorage.getItem('user'))
+            } else {
+                return false;
+            }
+        }
+    }
 }
--- frontend/src/Components/Header/Header.component.jsx
+++ frontend/src/Components/Header/Header.component.jsx
@@ -1,21 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 import { IconContext } from "react-icons";
 import classes from './Header.module.scss'
 import { Sidebar } from "../../Data/MenuBar";
 
+import { isAuth, signOut } from "../../Actions/Auth";
+
 
 const Header = () => {
+
+
+
+    function useOutsideAlerter(ref) {
+        useEffect(() => {
+    
+            function handleClickOutside(event) {
+                if (ref.current && !ref.current.contains(event.target)) {
+                    setUserBar(false)
+                }
+            }
+            document.addEventListener("mousedown", handleClickOutside);
+            return () => {
+                document.removeEventListener("mousedown", handleClickOutside);
+            };
+        }, [ref]);
+    }
+
+    const wrapperRef = useRef(null);
+    useOutsideAlerter(wrapperRef);
+
     const [sidebar, setSidebar] = useState(false);
     const showSidebar = () => setSidebar(!sidebar);
+
+
+    const [userBar, setUserBar] = useState(false)
+    const showUserBar = () => setUserBar(!userBar)
+
+    const history = useNavigate();
+
     const walpaper = process.env.PUBLIC_URL + '/wallpaper/starry1.jpg'
     const walpaper2 = process.env.PUBLIC_URL + '/wallpaper/starry2.jpg'
+
     return (
-        <>
+        <div>
             <IconContext.Provider value={{ color: "#FFF" }}>
 
                 <div className={classes.navbar} style={{ backgroundImage: `url(${walpaper})` }}>
@@ -26,18 +57,50 @@ const Header = () => {
                     <Link to="/" className={classes.home_icon} title="Home Page">
                         <FaIcons.FaHome />
                     </Link>
-                    
+
 
                     <div className={classes.search_box}>
                         <input type="search" className={classes.search_inp} placeholder="Search..." />
                     </div>
 
-                    <Link to="/login" className={classes.accont_bar} title="Login And Register">
-                        <span className={classes.acount_title}>
-                            Login
-                        </span>
-                        <FaIcons.FaUser />
-                    </Link>
+                    {/* {JSON.stringify(isAuth())} */}
+                    {isAuth() ?
+
+                        <Link className={classes.accont_bar} title="Log out from your accont"
+
+                            onClick={() => showUserBar()}
+                        >
+                            <span className={classes.acount_title}>
+                                <FaIcons.FaSortDown />
+                                {isAuth().name}
+                            </span>
+                            <FaIcons.FaUser />
+                        </Link>
+                        :
+                        <Link to="/login" className={classes.accont_bar} title="Login And Register">
+                            <span className={classes.acount_title}>
+                                Login
+                            </span>
+                            <FaIcons.FaUser />
+                        </Link>
+                    }
+
+                    {
+                        userBar ?
+                            <div className={classes.user_bar}
+                                ref={wrapperRef}
+                            >
+                                <ul>
+                                    <li>profile</li>
+                                    <li onClick={() => signOut(() => {
+                                        showUserBar();
+                                        history('/login');
+                                    })}>log out</li>
+                                </ul>
+                            </div>
+                            :
+                            ''
+                    }
 
                 </div>
 
@@ -65,7 +128,7 @@ const Header = () => {
 
 
             </IconContext.Provider>
-        </>
+        </div>
     );
 
 }
