eb009e229fabfb50a83f1ba56edaa2824e2e7e38
amir_tardasti <amir.tardasti7@gmail.com>
2023-06-26 15:05:06 +0430

    Continuation about create blog...

--- frontend/src/Components/CRUD/Blogs/createAndEdit/BlogCE.component.jsx
+++ frontend/src/Components/CRUD/Blogs/createAndEdit/BlogCE.component.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, json } from "react-router-dom";
 import ReactQuill, { Quill } from 'react-quill';
 
 import { index as getTags } from "../../../../Actions/Tag";
@@ -8,15 +8,64 @@ import { store } from "../../../../Actions/Blog";
 
 import '../../../../Style/quill.css'
 import classes from './BlogCE.module.scss'
+import { ToolbarOption, formats } from "../../../../Data/Quill";
+
 
 export const CreateBlogComponent = () => {
 
+    const [body, setBody] = useState(null);
+    const [title, setTitle] = useState();
+    const [file, setFile] = useState();
 
+    const blogFromLocalStorage = () => {
+        if (typeof window === 'undefined') {
+            return false
+        }
+        const getBlogFromLocalStorage = localStorage.getItem('blog');
 
+        if (getBlogFromLocalStorage && getBlogFromLocalStorage !== null) {
+            if (window.confirm('Do you want to continue your unfinished blog?')) {
+                return JSON.parse(localStorage.getItem('blog'))
+            } else {
+                localStorage.removeItem('blog')
+                document.getElementsByClassName("ql-editor")[0].innerHTML = null;
+            }
+        } else {
+            return false
+        }
+    }
+
+    useEffect(() => {
+        setBody(blogFromLocalStorage())
+    }, [window])
+
+    const changeHandler = name => e => {
+        switch (name) {
+            case 'title':
+                setTitle(e.target.value)
+                break;
+            case 'photo':
+                setFile(e.target.files[0])
+                break;
+            case 'body':
+                setBody(e)
+                if (typeof window !== 'undefined') {
+                    localStorage.setItem('blog', JSON.stringify(e))
+                }
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+    const cancleForm = () => {
+        localStorage.removeItem('blog')
+    }
 
     const formSubmitHandler = (e) => {
         e.preventDefault();
-        console.log(0);
 
     }
 
@@ -28,7 +77,7 @@ export const CreateBlogComponent = () => {
                         <h1 > Create new blog </h1>
                     </div>
                     <div className={classes.cancle}>
-                        <Link to="/admin/blogs" > Cancle </Link>
+                        <Link onClick={cancleForm} to="/admin/blogs" > Cancle </Link>
                     </div>
                 </div>
                 <div className={classes.content_box}>
@@ -36,12 +85,13 @@ export const CreateBlogComponent = () => {
 
                         <div className={classes.form_input}>
                             <label htmlFor="title">Title</label>
-                            <input name="title" type="text" placeholder="enter title..." />
+                            <input onChange={changeHandler('title')} value={title || ''} name="title" type="text" placeholder="enter title..." />
                             <button>Save</button>
                         </div>
 
+                        <label className={classes.body_label} htmlFor="body">Body</label>
                         <div className={classes.form_quill}>
-                            <ReactQuill placeholder="write something amazing..." />
+                            <ReactQuill value={body || ''} onChange={changeHandler('body')} modules={CreateBlogComponent.modules} name="body" formats={CreateBlogComponent.formats} placeholder="write something amazing..." />
                         </div>
 
                     </form>
@@ -53,6 +103,11 @@ export const CreateBlogComponent = () => {
 
 
 }
+CreateBlogComponent.modules = {
+    toolbar: ToolbarOption,
+}
+
+CreateBlogComponent.formats = formats;
 
 
 export const EditBlogComponent = () => { }
