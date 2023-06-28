43a375aeb6d7d11a4df183a8949d1202982398f8
amir_tardasti <amir.tardasti7@gmail.com>
2023-06-28 19:11:31 +0430

    update config

--- frontend/src/config_example.js
+++ frontend/src/config_example.js
@@ -1,4 +1,5 @@
 export const API = "";
 export const APP_NAME = "";
-export const MAX_PHOTO_SIZE = ""
-export const DOMAIN_DEVELOPMENT = ""
+export const MAX_PHOTO_SIZE = "";
+export const DOMAIN_DEVELOPMENT = "";
+export const DOMAIN_PRODUCTION = "";
