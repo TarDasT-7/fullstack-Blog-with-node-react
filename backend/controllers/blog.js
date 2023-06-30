b5c857e699e96502bf4f1828fa153ffd60453ff8
amir_tardasti <amir.tardasti7@gmail.com>
2023-06-30 13:36:38 +0430

    nothing :)

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -18,7 +18,7 @@ export const index = (req, res) => {
         .populate('categories', '_id name slug')
         .populate('tags', '_id name slug')
         .populate('postedBy', '_id name username')
-        .select('_id title slug excerpt mdes categories tags postedBy createdAt updatedAt')
+        .select('_id title slug excerpt categories tags postedBy createdAt updatedAt')
         .then(data => {
             res.json(data);
         });
